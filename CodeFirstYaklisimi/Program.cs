3e6fdc08ff378ca8e835a663683957715617fc1c
Ömer Kahveci <omerkahveci53@hotmail.com>
2022-06-18 14:12:48 +0300

    Add project files.

---
+++ CodeFirstYaklisimi/Program.cs
@@ -0,0 +1,14 @@
+﻿
+using CodeFirstYaklisimi;
+using CodeFirstYaklisimi.DAL;
+using Microsoft.EntityFrameworkCore;
+
+Initializer.Build();
+using (var _context= new AppDpContext())
+{
+    var products = await _context.Products.ToListAsync();
+    products.ForEach(p =>
+    {
+        Console.WriteLine($"{p.Id}: {p.Name}- {p.Stock}");
+    });
+}
