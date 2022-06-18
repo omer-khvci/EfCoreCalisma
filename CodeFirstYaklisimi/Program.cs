f0a432a57fcf08afba12d219aad8d7c3c01be07a
Ömer Kahveci <omerkahveci53@hotmail.com>
2022-06-18 15:13:04 +0300

    EfCoredaki State Kavramını Ogrendim

--- CodeFirstYaklisimi/Program.cs
+++ CodeFirstYaklisimi/Program.cs
@@ -6,9 +6,19 @@ using Microsoft.EntityFrameworkCore;
 Initializer.Build();
 using (var _context= new AppDpContext())
 {
-    var products = await _context.Products.ToListAsync();
-    products.ForEach(p =>
-    {
-        Console.WriteLine($"{p.Id}: {p.Name}- {p.Stock}");
-    });
+    //var products = await _context.Products.ToListAsync();
+    //products.ForEach(p =>
+    //{
+    //    var state = _context.Entry(p).State;
+    //    Console.WriteLine($"{p.Id}: {p.Name}- {p.Stock} state: {state}");
+    //});
+    var product = await _context.Products.FirstAsync();
+    Console.WriteLine($"ilk state {_context.Entry(product).State}");
+    _context.Remove(product);
+ 
+   // Console.WriteLine($"ilk state {_context.Entry(newProduct).State}");
+    //await _context.AddAsync(newProduct);
+    //Console.WriteLine($"ilk state {_context.Entry(newProduct).State}");
+    await _context.SaveChangesAsync();
+    Console.WriteLine($"unchanced -- {_context.Entry(product).State}");
 }
